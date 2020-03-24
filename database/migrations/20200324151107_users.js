d0742a5360513c73f151855d0f8979ed9fc1ac56
cedric-winbush <cawinbushjr@gmail.com>
2020-03-24 15:15:38 -0500

    added department to schema of table

--- database/migrations/20200324151107_users.js
+++ database/migrations/20200324151107_users.js
@@ -4,7 +4,8 @@ exports.up = function(knex) {
     users.increments();
 
     users.string('username', 128).notNullable().unique();
-    users.string('password', 128).notNullable()
+    users.string('password', 128).notNullable();
+    users.string('department', 128).notNullable();
 
   })
   
